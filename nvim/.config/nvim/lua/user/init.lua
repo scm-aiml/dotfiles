fcd18fcadaed08a000b1da4243485397251bb790
scm <scm>
2024-03-15 05:42:52 -0400

    lspkind

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -594,7 +594,7 @@ require("lazy").setup({
 			vim.cmd.colorscheme("everforest")
 
 			-- You can configure highlights by doing something like
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.hi("Comment gui=none") -- Not sure why but these prevents signature hinting
 		end,
 	},
 
