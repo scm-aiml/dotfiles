33a39af901e43387be5dafdfd3ad90e4603d775a
Shane Moran <124100101+scm-aiml@users.noreply.github.com>
2024-03-17 08:34:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ function check_prog() {
 check_prog stow
 check_prog curl
 
+# Define directories to remove
+directories=(~/.local/state/nvim ~/.local/share/nvim ~/.config/nvim)
+
+# Loop through each directory and remove it
+for dir in $directories; do
+    if [[ -d $dir ]]; then # Check if directory exists
+        echo "Removing $dir"
+        rm -rf $dir
+    else
+        echo "$dir does not exist."
+    fi
+done
+
 CONFIGDIR="$HOME/.config"
 
 # Check if the directory does not exist
